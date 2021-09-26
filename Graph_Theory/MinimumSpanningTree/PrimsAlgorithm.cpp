c0cba7ce0aee9c5ed37fb6ca79e98a87970a9292
Atharva Kulkarni <katharva606@gmail.com>
2021-09-25 22:35:32 -0700

    Prims Corrected;

--- Graph_Theory/MinimumSpanningTree/PrimsAlgorithm.cpp
+++ Graph_Theory/MinimumSpanningTree/PrimsAlgorithm.cpp
@@ -8,12 +8,11 @@ int PrimsAlgorithm(vector<pair<int,int>>adj[],int n,int src){
     vector<bool>mst(n,false);
     vector<int>parent(n,-1);
     key[0] = 0;
-    mst[0] = true;
     for(int count = 1; count < n; ++count){
             //1st
             int mini_val = INT_MAX, mini_idx = -1;
             for(int i = 1 ; i < n ; i++){
-                if(mst[i] == false && mini_val < key[i]){
+                if(mst[i] == false && mini_val > key[i]){
                     mini_val = key[i];
                     mini_idx = i;
                 }
@@ -38,23 +37,22 @@ int PrimsAlgorithm(vector<pair<int,int>>adj[],int n,int src){
 //     vector<bool>mst(n,false);
 //     vector<int>parent(n,-1);
 //     key[0] = 0;
-//     mst[0] = true;
 //     priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
 //     pq.push({0,0});
 //     for(int count = 1; count < n; ++count){
-//         int mini_idx = INT_MAX, mini_val = 0;
+//         int mini_idx =0;
 //         mini_idx = pq.top().second;
 //         pq.pop();
 //             //1st
 //         mst[mini_idx] = true;
 //         //mini_idx is a node now 
 //         for(auto it : adj[mini_idx]){
-//             int value = it.first;
+//             int node = it.first;
 //             int weight = it.second;
-//             if(mst[value] == false && weight < key[value]){
-//                 key[value] = weight;
-//                 pq.push({key[value],value});
-//                 parent[value] = mini_idx;
+//             if(mst[node] == false && weight < key[node]){
+//                 key[node] = weight;
+//                 pq.push({key[node],node});
+//                 parent[node] = mini_idx;
 //             }
 //         }
 //     }
@@ -64,12 +62,12 @@ int PrimsAlgorithm(vector<pair<int,int>>adj[],int n,int src){
 int main(){
     int n,m,wt;
     cin >> n >> m;
-    vector<pair<int,int>>adj[];
+    vector<pair<int,int>>adj[n];
     for(int i = 0 ; i < m; i++){
         int u,v,wt; 
         cin >> u >> v >> wt;
         //Undirected Graph
-        adj[u].push_back({v,w});
+        adj[u].push_back({v,wt});
         adj[v].push_back({u,wt});
     }
     int src = 0;
